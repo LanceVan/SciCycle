e0e1af3346aaa2d9cbddcf5ad5139459b4eebc00
LanceVan <zjvzhengs@gmail.com>
2015-04-21 17:02:22 +0800

    Add Vector Parameter

--- LagrangeInterpolation.py
+++ LagrangeInterpolation.py
@@ -19,13 +19,23 @@ class LagrangeInterpolation:
         self.size = sizex[0]
 
     def predict(self, x):
+        if type(x) is not np.ndarray:
+            raise TypeError("Type of Parameter should be numpy.ndarray")
+
+        y = []
         l = np.empty(self.size)
         li = np.empty(self.size - 1)
-        lNume = np.array([x] * self.size) - self.x
-        for i in range(self.size):
-            lDeno = np.array([self.x[i]] * self.size) - self.x
-            li[ : i] = lNume[ : i] / lDeno[ : i]
-            li[i : ] = lNume[i + 1 : ] / lDeno[i + 1 : ]
-            l[i] = li.cumprod()[-1]
+        for xi in x:
+            lNume = np.linspace(xi, xi, self.size) - self.x
+            for i in range(self.size):
+                lDeno = np.array([self.x[i]] * self.size) - self.x
+                li[ : i] = lNume[ : i] / lDeno[ : i]
+                li[i : ] = lNume[i + 1 : ] / lDeno[i + 1 : ]
+                l[i] = li.cumprod()[-1]
 
-        return sum(self.y * l)
+            y.append(sum(self.y * l))
+
+        return np.array(y)
+
+    def remainder(self):
+        return self.y - self.predict(self.x)
